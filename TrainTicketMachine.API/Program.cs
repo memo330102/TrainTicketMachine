2a4c64aabc12e4e9891ec8b262ffa81311a7fa86
Mehmet Aksak <1995mehmetaksak@gmail.com>
2025-02-19 17:06:55 +0100

    api versiyoning added

--- TrainTicketMachine.API/Program.cs
+++ TrainTicketMachine.API/Program.cs
@@ -6,6 +6,7 @@ using TrainTicketMachine.Infrastructure.Contracts;
 using TrainTicketMachine.Infrastructure.Providers;
 using TrainTicketMachine.Infrastructure.Repositories;
 using TrainTicketMachine.Infrastructure.BackgroundServices;
+using TrainTicketMachine.Application.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,9 +24,22 @@ builder.Services.AddScoped<IStationHelper, StationHelper>();
 builder.Services.AddHostedService<StationCacheRefresher>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+
+#region Services.Swagger.ApiVersioning
+builder.Services.AddApiVersioning(o =>
+{
+    o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.ReportApiVersions = true;
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+#endregion
 
 var app = builder.Build();
 
---
+++ TrainTicketMachine.Application/Configurations/ConfigureSwaggerOptions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace TrainTicketMachine.Application.Configurations
+{
+    public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _apiVersionDescriptionProvider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider) => _apiVersionDescriptionProvider = apiVersionDescriptionProvider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _apiVersionDescriptionProvider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateVersionInfo(description));
+            }
+        }
+
+        public void Configure(string? name, SwaggerGenOptions options)
+        {
+            Configure(options);
+        }
+
+        private OpenApiInfo CreateVersionInfo(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "TrainTicketMachine Api Gateway",
+                Version = description.ApiVersion.ToString()
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
+        }
+    }
+}
