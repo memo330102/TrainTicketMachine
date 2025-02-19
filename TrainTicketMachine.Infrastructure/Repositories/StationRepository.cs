36d5e73da10b1dc61b963c5b38eebd0722b9be16
Mehmet Aksak <1995mehmetaksak@gmail.com>
2025-02-19 12:42:16 +0100

    Solid open closed princible added for providers of stations

--- TrainTicketMachine.Infrastructure/Repositories/StationRepository.cs
+++ TrainTicketMachine.Infrastructure/Repositories/StationRepository.cs
@@ -7,16 +7,25 @@ namespace TrainTicketMachine.Infrastructure.Repositories
 {
     public class StationRepository : IStationRepository
     {
-        private readonly IStationDataSource _dataSource;
-        private static List<StationDataSourceResponse> _cachedStations = new List<StationDataSourceResponse>();
-        public StationRepository(IStationDataSource remoteStationProvider)
+        private readonly IEnumerable<IStationDataSource> _dataSources;
+
+        public StationRepository(IEnumerable<IStationDataSource> dataSources)
         {
-            _dataSource = remoteStationProvider;
+            _dataSources = dataSources;
         }
 
         public async Task<List<StationDataSourceResponse>> SearchStationsAsync(string query)
         {
-            return await _dataSource.LoadStationsAsync();
+            var dataTasks = _dataSources.Select(ds => ds.LoadStationsAsync());
+            var results = await Task.WhenAll(dataTasks);
+
+            var combinedStations = results
+                                   .SelectMany(stationList => stationList)
+                                   .GroupBy(s => s.stationName, StringComparer.OrdinalIgnoreCase)
+                                   .Select(g => g.First())
+                                   .ToList();
+
+            return combinedStations;
         }
     }
 }
