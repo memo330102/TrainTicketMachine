086cc0156ac0edf5b9440c3fb58f3985d08ce939
Mehmet Aksak <1995mehmetaksak@gmail.com>
2025-02-26 09:48:48 +0100

    BackendService and Caching folder added correctly

---
+++ TrainTicketMachine.Caching/CacheService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace TrainTicketMachine.Caching
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IMemoryCache _cache;
+        public CacheService(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+        public async Task<T> GetCacheValueAsync<T>(string key)
+        {
+            if (_cache.TryGetValue(key, out var value))
+            {
+                return JsonSerializer.Deserialize<T>(value.ToString()!);
+            }
+            return default;
+        }
+
+        public async Task SetCacheValueAsync<T>(string key, T value, TimeSpan expiration)
+        {
+            var json = JsonSerializer.Serialize(value);
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expiration
+            };
+            _cache.Set(key, json, cacheEntryOptions);
+        }
+
+        public async Task RemoveCacheValueAsync(string key)
+        {
+            _cache.Remove(key);
+        }
+    }
+}
---
+++ TrainTicketMachine.Caching/ICacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainTicketMachine.Caching
+{
+    public interface ICacheService
+    {
+        Task<T> GetCacheValueAsync<T>(string key);
+        Task SetCacheValueAsync<T>(string key, T value, TimeSpan expiration);
+        Task RemoveCacheValueAsync(string key);
+    }
+}
